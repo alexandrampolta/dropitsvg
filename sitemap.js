f589bb2d29a48ef82cfc01751f5faa688dab5028
my name <myemailid@domain.com>
2021-11-12 16:03:26 +0200

    moringswrrds

--- sitemap.js
+++ sitemap.js
@@ -21,7 +21,7 @@ var rangee = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min +
 var num1 = 1363
 var num2 = 1782
 
-var sitemapp = 2
+var sitemapp = 3
 
 var a = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">`;
