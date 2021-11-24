e9b3487f799e6e608502fcad26320d527cf67699
my name <myemailid@domain.com>
2021-11-25 01:19:58 +0200

    update for hellosvg

--- sitemap.js
+++ sitemap.js
@@ -22,50 +22,50 @@ var databases = mysql.createConnection({
 // })
 import fs from "fs"
 
-databases.query('select * from productss where id >1784' , function (error, results, fields) {
-console.log(error);
-var a = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">`;
+// databases.query('select * from productss where id >1784' , function (error, results, fields) {
+// console.log(error);
+// var a = `<?xml version="1.0" encoding="UTF-8"?>
+// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">`;
 
-results.forEach(function(prod){
-try{
-var idd = prod.id
-var netimg = prod.image
-  if(idd>=2664){
-  a = a+"\n"+`</urlset>`
- fs.writeFile("./public/sitemapp4.xml",a, 'utf8', err => {
- if (err) throw err;
-console.log('File has been saved!');
-});
+// results.forEach(function(prod){
+// try{
+// var idd = prod.id
+// var netimg = prod.image
+//   if(idd>=2664){
+//   a = a+"\n"+`</urlset>`
+//  fs.writeFile("./public/sitemapp4.xml",a, 'utf8', err => {
+//  if (err) throw err;
+// console.log('File has been saved!');
+// });
 
 
 
-  }else{
-    var minit  = prod.title.toString().replace(/,/g,"").replace(/ /g,"-").replace(/:/g,"").replace(/!/g,"").replace(/--/g,"-").replace(/&/g,"-")
-    a = a+"\n"+`<url>
-   <loc>https://dropitsvg.com/product/`+idd+`/`+minit+`</loc>
-   <changefreq>weekly</changefreq>
-   <priority>1.0</priority>
-   </url>`
+//   }else{
+//     var minit  = prod.title.toString().replace(/,/g,"").replace(/ /g,"-").replace(/:/g,"").replace(/!/g,"").replace(/--/g,"-").replace(/&/g,"-")
+//     a = a+"\n"+`<url>
+//    <loc>https://dropitsvg.com/product/`+idd+`/`+minit+`</loc>
+//    <changefreq>weekly</changefreq>
+//    <priority>1.0</priority>
+//    </url>`
 
-databases.query('UPDATE productss SET image = "https://dropitsvg.com/images/dropitsvghl/'+netimg+'" where id ="'+idd+'"' , function (error, results, fields) {
-console.log(error);
-console.log(idd +"updated");
-})
+// databases.query('UPDATE productss SET image = "https://dropitsvg.com/images/dropitsvghl/'+netimg+'" where id ="'+idd+'"' , function (error, results, fields) {
+// console.log(error);
+// console.log(idd +"updated");
+// })
 
-  }
+//   }
     
 
-}catch(err){
+// }catch(err){
 
-}
-
-
+// }
 
 
 
-});
-})
+
+
+// });
+// })
 
 
 
@@ -126,3 +126,35 @@ console.log(idd +"updated");
 
 // }
 // },200)
+databases.query('select * from productss where image like "%dropitsvghl%"' , function (error, results, fields) {
+  a = "";
+  results.forEach(function(itemo){
+    if(id>=2664){
+ fs.writeFile("./public/datawewant.txt",a, 'utf8', err => {
+ if (err) throw err;
+console.log('File has been saved!');
+});
+    }else{
+      var title = itemo.title;
+      var imagename = itemo.image;
+      var id = itemo.id;
+
+     a = a+"\n"+id+"  "+title+"   "+imagename;
+
+    }
+   
+
+
+
+
+
+
+  });
+
+
+
+
+
+});
+
+
